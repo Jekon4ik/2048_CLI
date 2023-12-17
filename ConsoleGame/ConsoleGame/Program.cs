6701d16ef3e81b19494ab715ee67e69d7eec21ef
Саврацький Євгеній <ghfswhrh@gmail.com>
2023-12-17 01:13:06 +0100

    basic structure started

--- ConsoleGame/ConsoleGame/Program.cs
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+namespace ConsoleGame
+{
+    class Program
+    {
+        public static int[,] Field = new int[4,4];
+        public static int Score = 0;
+        public static bool EndGame = false;
+        static void Main(string[] args)
+        {
+            while(!EndGame)
+            {
+
+            }
+        }
+
+
+        public void RightSwap()
+        {
+
+        }
+        public void LeftSwap()
+        {
+
+        }
+        public void UpSwap()
+        {
+
+        }
+        public void DownSwap()
+        {
+
+        }
+
+    }
+}
