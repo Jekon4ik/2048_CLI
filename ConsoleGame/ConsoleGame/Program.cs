ee7b151571d2500ae7e85c27949c8ab9d56cc0e1
Jekon4ik <ghfswhrh@gmail.com>
2023-12-25 15:23:29 +0100

    logic for rigt and left swipes added

--- ConsoleGame/ConsoleGame/Program.cs
+++ ConsoleGame/ConsoleGame/Program.cs
@@ -8,20 +8,99 @@ namespace ConsoleGame
         public static bool EndGame = false;
         static void Main(string[] args)
         {
-            while(!EndGame)
-            {
+            Field[0,3] = 2;
+            Field[0,2] = 2;
+            Field[0,1] = 0;
+            Field[0,0] = 0;
+            Field[1, 3] = 2;
+            Field[1, 2] = 2;
+            Field[1, 1] = 2;
+            Field[1, 0] = 2;
+            OutputGameField();
+            Console.ReadLine();
+            LeftSwap();   
+            OutputGameField();
+            
 
+        }
+        public static void OutputGameField()
+        {
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); j++)
+                {
+                    Console.Write(Field[i, j] + " ");
+                }
+                Console.WriteLine();
             }
         }
-
-
-        public void RightSwap()
+        public static void RightSwap()
         {
+           for(int k =0; k < 3; k++)
+           {
+                for (int i = Field.GetLength(0) - 1; i >= 0; i--)
+                {
+                    for (int j = Field.GetLength(1) - 1; j > 0; j--)
+                    {
+                        if (Field[i, j] == Field[i, j - 1])
+                        {
+                            Field[i, j] = Field[i, j] + Field[i, j - 1];
+                            Field[i, j - 1] = 0;
+                        }
 
+                    }
+                }
+           }
+           for(int i = 0; i< Field.GetLength(0); i++)
+            {
+                for (int j =1; j < Field.GetLength(1); j++)
+                {
+                    if (Field[i,j-1] != 0)
+                    {
+                        if (Field[i, j] == 0)
+                        {
+                            Field[i, j] = Field[i, j - 1];
+                            Field[i, j - 1] = 0;
+                        }
+                    }
+                }
+            }
+          
         }
-        public void LeftSwap()
+        public static void LeftSwap()
         {
+            for (int k = 0; k < 3; k++)
+            {
+                for (int i = 0; i < Field.GetLength(0); i++)
+                {
+                    for (int j = 0; j <Field.GetLength(1)-1; j++)
+                    {
+                        //Console.WriteLine($"i = {i} j = {j}");
+                        if (Field[i, j] == Field[i, j + 1])
+                        {
+                            Field[i, j] = Field[i, j] + Field[i, j + 1];
+                            Field[i, j + 1] = 0;
+                        }
 
+                    }
+                }
+                //Console.WriteLine("gey");
+            }
+            for (int i = Field.GetLength(0)-1; i >= 0; i--)
+            {
+                for (int j = Field.GetLength(1)-2; j >=0; j--)
+                {
+                    Console.WriteLine($"i = {i} j = {j}");
+                    if (Field[i, j + 1] != 0)
+                    {
+                        if (Field[i, j] == 0)
+                        {
+                            Field[i, j] = Field[i, j + 1];
+                            Field[i, j + 1] = 0;
+                        }
+                    }
+                }
+            }
         }
         public void UpSwap()
         {
